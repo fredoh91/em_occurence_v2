b7d0aeb3879cdf3dfced5eba23dd6ab63adb447e
fredoh91 <fredoh@gmail.com>
2020-08-04 16:35:52 +0200

    formulaire de recherche de medoc et affichage sous forme de tableau bootstrap

--- src/Controller/RechercheMedicamentController.php
+++ src/Controller/RechercheMedicamentController.php
@@ -4,25 +4,74 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\EmOccProduit;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\EmOccProduitV2;
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\Common\Persistence\ObjectManager;
+//use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Form\FormInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class RechercheMedicamentController extends AbstractController
 {
     /**
      * @Route("/recherche_medicament", name="recherche_medicament")
      */
-    public function searchProduit(Request $request, ObjectManager $manager) {
-        $EmOccProduit = new EmOccProduit();
-        
-        $form = $this->createFormBuilder($EmOccProduit)
-                     ->add('produit')
-                     ->getForm();
-        return $this->render('recherche_medicament/recherche_medicament.html.twig',[
-            'formEmOccProduit' => $form->createView()
+    public function searchProduit(Request $request, EntityManagerInterface $em):Response
+        {
+            $form =$this->createFormBuilder()
+                        ->setMethod('GET')
+                        ->add('produit',TextType::class)
+                        ->add('submit', SubmitType::class, ['label' => 'Recherche d\'un produit'])
+                        ->getForm()
+                    ;
+            $form->handleRequest($request);
+            if ($form->isSubmitted() && $form->isValid()) {
+                $data = $form->getData();
+                
+                
+        //        $repo = $em->getRepository('App\Entity\EmOccProduitV2');
+                $repo = $em->getRepository(EmOccProduitV2::class);
+        //        $tousproduits = $repo->findAll();
+                $tousproduits = $repo->findBy(array('produit' => $data['produit']));
+                
+                
+                
+                
+                
+                 return $this->render('recherche_medicament/recherche_medicament.html.twig', [
+                        'form_rech_med' => $form->createView(),
+                        'produits'=> $tousproduits
+                ]);
+                
+                
+            }
+            
+            return $this->render('recherche_medicament/recherche_medicament.html.twig', [
+            'form_rech_med' => $form->createView(),
+            'produits'=> 'Pas de produit'
         ]);
+            
+            
+////        $repo = $em->getRepository('App\Entity\EmOccProduitV2');
+//        $repo = $em->getRepository(EmOccProduitV2::class);
+////        $tousproduits = $repo->findAll();
+//        $tousproduits = $repo->findBy(array('produit' => 'VOLTARENPLAST'));
+//         return $this->render('recherche_medicament/recherche_medicament.html.twig', [
+//            'controller_name' => 'RechercheMedicamentController',
+//             'produits'=> $tousproduits
+//        ]);       
+        
+//        $EmOccProduitV2 = new EmOccProduitV2();
+//        dump($EmOccProduitV2);
+//        $form = $this->createFormBuilder($EmOccProduitV2)
+//                     ->add('produit')
+//                     ->getForm();
+//        return $this->render('recherche_medicament/recherche_medicament.html.twig',[
+//            'formEmOccProduitV2' => $form->createView()
+//        ]);
         
     }
     
