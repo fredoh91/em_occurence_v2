0d56b53771892cd665aa006fe51e950447783d7c
fredoh91 <fredoh@gmail.com>
2020-11-10 23:49:33 +0100

    ajout d une alerte au lancement le maj de la base

--- config/envMsAccess.php
+++ config/envMsAccess.php
@@ -1,5 +1,9 @@
 <?php
+
+//var_dump('%env(APP_ENV)%');
+//die();
 return array (
+    
     "dbAccessName" => "D:\Users\Frannou\Dev\Access\EM\Extractions_EM\DATA\Proto_2013_be.accdb;",
 //    "dbAccessName" => "G:\DM-SURV\VIGIL1\COMMUNDP\ERREURS MEDICAMENTEUSES\SIGNALEMENTS\Outil de saisie EM\DATA\Proto_2013_be.accdb;",
 );
